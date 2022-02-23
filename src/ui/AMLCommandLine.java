4d9a84e87357b4f4b7d1fd521659aef3aebc6f6c
ChrisItisdud <chris.reichel@gmx.net>
2022-02-23 23:35:47 +0100

    added comments to AMLCommandLine for readability

--- src/ui/AMLCommandLine.java
+++ src/ui/AMLCommandLine.java
@@ -11,19 +11,30 @@ public class AMLCommandLine {
 		Scanner scanner = new Scanner(System.in);
 		models.Automaton automaton = null;
 		while (true) {
+			//get command
 			String command = scanner.nextLine();
+			//"quit" command
 			if (command.startsWith("quit")) {
 				System.out.println("quitting...");
 				break;
-			} else if (command.startsWith("parse")) {
+			} 
+			//"parse" command
+			else if (command.startsWith("parse")) {
+				try {
 				automaton = AMLCompiler.parse(command.split(" ")[1]);
+				} catch (Exception e) {
+					System.out.println("Something went wrong while reading the file. Please check filename spelling and try again.");
+				}
 			}
+			//"check" command
 			else if (command.startsWith("check")) {
 				if (automaton == null)
 					System.out.println("Error: no automaton defined yet!");
 				else {
 					compiler.AMLRuntime runtime = new compiler.AMLRuntime(automaton, command.substring(6));
+					//LOGIC FOR DFAs
 					if (automaton.getType() == models.AutomatonType.DFA) {
+						//Step through word
 						models.RuntimeResponse state = null;
 						do {
 							state = runtime.stepDeterministic();
@@ -31,6 +42,7 @@ public class AMLCommandLine {
 									"Read: " + state.getChar() + ", Now entering state " + state.getState().getName());
 							scanner.nextLine();
 						} while (!state.isFinished());
+						//output result
 						if (!state.isWord() && state.getChar() == null) {
 							System.out.println("Failure: Word ended on non-final state " + state.getState().getName()
 									+ ". Word is not part of language.");
@@ -40,16 +52,18 @@ public class AMLCommandLine {
 						} else {
 							System.out.println("The given word " + command.substring(6) + " is part of language!");
 						}
+					//LOGIC FOR NFAs
 					} else if (automaton.getType() == models.AutomatonType.NFA) {
 						models.RuntimeResponse state = null;
 						try {
 							do {
+								//Check options for next state
 								models.IState[] options = runtime.chooseNonDeterministic();
-								if (options == null) {
+								if (options == null) {	//==failure
 									state = new models.RuntimeResponse(runtime.getCurr(), '#', true, false);
-								} else if (options.length == 1) {
+								} else if (options.length == 1) { //==deterministic clear path
 									state = runtime.stepNonDeterministic(options[0]);
-								} else {
+								} else { //==non-deterministic - let user decide
 									System.out.println("Please choose the most appropriate option:");
 									int i = 0;
 									for (models.IState s : options) {
@@ -72,9 +86,11 @@ public class AMLCommandLine {
 										+ state.getState().getName());
 								scanner.nextLine();
 							} while (!state.isFinished());
+						//finish via exception - hacky solution but works
 						} catch (exception.AMLRuntimeFinishedException e) {
 							state = new RuntimeResponse(e.getState(), e.getLetter(), true, e.isWord());
 						}
+						//output result
 						if (!state.isWord() && state.getChar() == null) {
 							System.out.println("Failure: Word ended on non-final state " + state.getState().getName()
 									+ ". Word is not part of language.");

97bb86e60760f83875718b3d2af5ee05ea1136dd
ChrisItisdud <chris.reichel@gmx.net>
2022-02-24 13:13:55 +0100

    implemented compiler for DPDAs and NPDAs

--- src/compiler/AMLCompiler.java
+++ src/compiler/AMLCompiler.java
@@ -78,7 +78,7 @@ public class AMLCompiler {
 				if (!line.startsWith("start: "))
 					throw new exception.AMLIllegalSyntaxException(
 							exception.AMLSyntaxExceptions.ERR_UNEXPECTED_IDENTIFIER);
-				if (type == AutomatonType.NFA || type == AutomatonType.NPDA) {
+				if (type == AutomatonType.NFA) {
 					String[] startsString = line.substring(7).split(", ");
 					for (String s : startsString) {
 						if (!states.containsKey(s))
@@ -140,11 +140,114 @@ public class AMLCompiler {
 				}
 				return new models.Automaton(starts, name, type);
 			} else {
-				//TODO: Implement compiler logic for DPDAs and NPDAs
-				return null;
+				//TODO: Implement compiler logic for DPDAs and NPDAs				
+				// start section states
+				line = br.readLine();
+				if (!line.equals("SECTION STATES"))
+					throw new exception.AMLIllegalSyntaxException(
+							exception.AMLSyntaxExceptions.ERR_SECTION_STATES_MISSING);
+				// grab states
+				// TODO: Check for duplicate state names
+				line = br.readLine();
+				if (!line.startsWith("states: "))
+					throw new exception.AMLIllegalSyntaxException(
+							exception.AMLSyntaxExceptions.ERR_UNEXPECTED_IDENTIFIER);
+				String[] statesString = line.substring(8).split(", ");
+				HashMap<String, models.IPDAState> states = new HashMap<>();
+				for (String s : statesString) {
+					if (states.containsKey(s))
+						throw new exception.AMLIllegalSyntaxException(
+								exception.AMLSyntaxExceptions.ERR_KEY_ALREADY_EXISTS);
+					switch (type) {
+					case DPDA:
+						states.put(s, new models.DPDAState(s));
+						break;
+					case NPDA:
+						states.put(s, new models.NPDAState(s));
+						break;
+					default:
+						throw new RuntimeException("Not Implemented Yet!");
+					}
+				}
+				// grab start state(s)
+				line = br.readLine();
+				ArrayList<models.IPDAState> startStates = new ArrayList<>();
+				if (!line.startsWith("start: "))
+					throw new exception.AMLIllegalSyntaxException(
+							exception.AMLSyntaxExceptions.ERR_UNEXPECTED_IDENTIFIER);
+				if (type == AutomatonType.NPDA) {
+					String[] startsString = line.substring(7).split(", ");
+					for (String s : startsString) {
+						if (!states.containsKey(s))
+							throw new exception.AMLIllegalSyntaxException(
+									exception.AMLSyntaxExceptions.ERR_UNKNOWN_STATE);
+						startStates.add(states.get(s));
+					}
+				} else {
+					String startsString = line.substring(7);
+					if (!states.containsKey(startsString))
+						throw new exception.AMLIllegalSyntaxException(exception.AMLSyntaxExceptions.ERR_UNKNOWN_STATE);
+					startStates.add(states.get(startsString));
+				}
+				// grab end states
+				line = br.readLine();
+				if (!line.startsWith("fin: "))
+					throw new exception.AMLIllegalSyntaxException(
+							exception.AMLSyntaxExceptions.ERR_UNEXPECTED_IDENTIFIER);
+				String[] endsString = line.substring(5).split(", ");
+				for (String s : endsString) {
+					if (!states.containsKey(s))
+						throw new exception.AMLIllegalSyntaxException(exception.AMLSyntaxExceptions.ERR_UNKNOWN_STATE);
+					states.get(s).setEndState(true);
+				}
+				// start section transitions
+				line = br.readLine();
+				if (!line.equals("SECTION TRANSITIONS"))
+					throw new exception.AMLIllegalSyntaxException(
+							exception.AMLSyntaxExceptions.ERR_SECTION_TRANSITIONS_MISSING);
+				// TODO: validate transitions before parsing
+				// parse transitions
+				line = br.readLine();
+				while (line != null) {
+					String[] transString = line.split(", ");
+					if(transString.length!=5) throw new exception.AMLIllegalSyntaxException(exception.AMLSyntaxExceptions.ERR_UNEXPECTED_IDENTIFIER);
+					if(transString[1].length()!=1 || transString[2].length()!=1) throw new exception.AMLIllegalSyntaxException(exception.AMLSyntaxExceptions.ERR_ILLEGAL_KEY_LENGTH);
+					if (!states.containsKey(transString[0]) || !states.containsKey(transString[3]))
+						throw new exception.AMLIllegalSyntaxException(exception.AMLSyntaxExceptions.ERR_UNKNOWN_STATE);
+					switch (type) {
+					case DPDA:
+						models.DPDAState state = (models.DPDAState) states.get(transString[0]);
+						Character[] stackTarget = toCharacterArray(transString[4]);
+						state.addTransition(transString[2].charAt(0), transString[1].charAt(0), states.get(transString[3]), stackTarget);
+						break;
+					case NPDA:
+						models.NPDAState nfastate = (models.NPDAState) states.get(transString[0]);
+						nfastate.addTransition(transString[2].charAt(0), transString[1].charAt(0), states.get(transString[2]), toCharacterArray(transString[4]));
+						break;
+					default:
+						break;
+					}
+
+					line = br.readLine();
+				}
+				models.IPDAState[] starts = new models.IPDAState[startStates.size()];
+				int i = 0;
+				for (models.IPDAState s : startStates) {
+					starts[i] = s;
+					i++;
+				}
+				return new models.Automaton(starts, name, type);
 			}
 		} catch (IOException e) {
 			throw new RuntimeException("Something went wrong while reading the file");
 		}
+	}
+	
+	public static Character[] toCharacterArray(String s) {
+		Character[] array = new Character[s.length()];
+		for(int i=0;i<array.length;i++) {
+			array[i]=s.charAt(i);
+		}
+		return array;
 	}
 }
--- src/models/Automaton.java
+++ src/models/Automaton.java
@@ -4,10 +4,19 @@ public class Automaton {
 	private final AutomatonType type;
 	private final String name;
 	private final IState[] start;
+	private final IPDAState[] pdaStart;
 	public Automaton(IState[] start, String name, AutomatonType type) {
 		this.start = start;
 		this.name = name;
 		this.type = type;
+		pdaStart = null;
+	}
+	
+	public Automaton(IPDAState[] pdaStart, String name, AutomatonType type) {
+		this.pdaStart = pdaStart;
+		this.name = name;
+		this.type = type;
+		start = null;
 	}
 	
 	public AutomatonType getType() {
@@ -18,5 +27,8 @@ public class Automaton {
 	}
 	public IState[] getStart() {
 		return start;
+	}
+	public IPDAState[] getPdaStart() {
+		return pdaStart;
 	}
 }
